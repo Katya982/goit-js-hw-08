682c4644e2b838149e0c13cc5bda0e5083c39b86
Katya982 <xamko98@gmail.com>
2022-08-19 15:08:55 +0300

    fix
    fix

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,33 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from "simplelightbox/dist/simple-lightbox.esm";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-console.log(galleryItems);
+const imgGallary = document.querySelector(".gallery");
+const  imgMarkup = creatGallary(galleryItems);
+imgGallary.insertAdjacentHTML('beforeend', imgMarkup);
+
+function creatGallary(galleryItems) {
+    return  galleryItems.map(({preview, original, description}) => {
+        return ` 
+        <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </div> 
+      `;
+    })
+    .join('');
+
+};
+
+   let gallery = new SimpleLightbox('.gallery a', {
+    captionData: 'alt',
+    captionType: 'attr',
+	  captionDelay: 250,
+});
+    gallery.on('show.simplelightbox');
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
+import throttle from 'lodash.throttle';
 
+const refs = {
+     form: document.querySelector('.feedback-form'),
+     email: document.querySelector('.feedback-form input'),
+     message: document.querySelector(".feedback-form  textarea"),
+};
+     refs.form.addEventListener("input", throttle(onTextareaInput, 500));
+     refs.form.addEventListener("submit", onFormSubmit);
+  
+     const STORAGE_KEY = "feedback-form-state";                           
+     const formData = {};
+
+     function onTextareaInput(event) {
+       formData[event.target.name] = event.target.value;
+       localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+   };
+                                                                     
+     function onFormSubmit(event) {
+        event.preventDefault(); 
+        refs.email = event.currentTarget.email.value;
+        refs.message = event.currentTarget.message.value;
+
+       if (refs.email === '' || refs.message === '') {
+          return alert('All fields must be filled!');
+        }
+          console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
+          event.currentTarget.reset();                                      
+          localStorage.removeItem(STORAGE_KEY);      
+        
+   };
+      function populateTextarea() {
+       const savedMessage = JSON.parse(localStorage.getItem(STORAGE_KEY)); 
+       if (savedMessage) {
+         refs.email.value = savedMessage.email;
+         refs.message.value = savedMessage.message;
+    }
+   };
+       populateTextarea();
